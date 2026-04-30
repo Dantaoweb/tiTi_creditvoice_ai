443afa583adc1de9982328fecafea2a7ee50d0c7
Dantaoweb <141657781+Dantaoweb@users.noreply.github.com>
2026-04-30 23:18:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,10 +38,9 @@ app = FastAPI()
 # ---------------- GLOBAL ----------------
 processed_messages = set()
 
-WHATSAPP_TOKEN = "YOUR_ACCESS_TOKEN"
-PHONE_NUMBER_ID = "YOUR_PHONE_NUMBER_ID"
+WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 VERIFY_TOKEN = "creditvoice_verify_123"
-
 # ---------------- WHATSAPP SEND ----------------
 def send_whatsapp_message(to, message):
     url = f"https://graph.facebook.com/v18.0/{PHONE_NUMBER_ID}/messages"
