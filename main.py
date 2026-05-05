80ab8c23c4899434f2d269bfb6140b77d47cdf24
Dantaoweb <141657781+Dantaoweb@users.noreply.github.com>
2026-05-05 07:22:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -127,12 +127,14 @@ def get_balance(db, customer_id):
     total_buy =db.query(
         
         func.coalesce(func.sum(Transaction.amount), 0)).filter(
-     Transaction.customer_id == customer_id),Transaction. type == "BUY"
+     Transaction.customer_id == customer_id
+        ,Transaction. type == "BUY"
     ).scalar()
 
    total_pay = db.query(
        func.coalesce(func.sum(Transaction.amount), 0)).filter(
-     Transaction.customer_id == customer_id),Transaction. type == "PAY" 
+     Transaction.customer_id == customer_id,
+       Transaction. type == "PAY" 
     ).scalar()
 
         return total_buy - total_pay
