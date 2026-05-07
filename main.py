ba11119cd7e7543d8818b81d0bf281c15af85a8c
Dantaoweb <141657781+Dantaoweb@users.noreply.github.com>
2026-05-07 18:57:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -93,8 +93,10 @@ def parse_message(text):
 
     if "balance" in text:
         return {"type": "BALANCE"}
-        clean_text = text.replace(", ",  "")
-        numbers = re.findall(r"\d+", clean_text)
+        
+    clean_text = text.replace(", ",  "")
+    numbers = re.findall(r"\d+", clean_text)
+    
     if not numbers:
         return None
 
